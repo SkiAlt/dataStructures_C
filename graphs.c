ef55c0d2509d50a7d977908a699943074598d42d
Skia <sairagav2003@gmail.com>
2023-02-22 20:40:13 +0530

    added DFS and BFS(rekt)

--- graphs.c
+++ graphs.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define GRAPHSIZE 15
+#define SIZE 15
 
 int adjacenyMatrix[GRAPHSIZE][GRAPHSIZE] = {0};
+int visited[GRAPHSIZE]= {0};
 
 void initAdjacencyMatrix(int n)
 {
@@ -34,13 +36,77 @@ void printAdjacencyMatrix(int n){
 		printf("\n");
 	}
 }
+int f, r, count;
+int q[SIZE]; 
+void enqueue(int item)
+{
+	r = (r + 1) % SIZE;
+	q[r] = item;
+	count++;
+}
+int dequeue()
+{
+	int preF = f;
+	f = (f + 1) % SIZE;
+	count--;
+	return q[preF];
+}
+void BFS(int v, int n)
+{
+	for(int i=0;i<n;i++)
+		if(adjacenyMatrix[v][i] && !visited[i] )
+			enqueue(i);
+	if(count>0)
+	{
+		visited[v]=1;
+		printf("%d  ", v + 1);
+		int next=dequeue();
+		BFS(next,n);
+	}
+}
+
+void DFS(int v, int n)
+{
+	visited[v] = 1;
+	printf("%d  ",v+1);
+	for (int i = 0; i < n; i++)
+		if (adjacenyMatrix[v][i] && !visited[i] )
+			DFS(i,n);
+}
+
+void clearVisited()
+{
+	for(int i=0;i<GRAPHSIZE;i++)
+		visited[i]=0;
+}
 
 int main()
 {
-    printf("Enter number of nodes:  ");
-    int n;
-    scanf("%d", &n);
-    initAdjacencyMatrix(n);
-    printAdjacencyMatrix(n);
-    return 0;
+	printf("Enter number of nodes:  ");
+	int n,ch,v;
+	scanf("%d", &n);
+	initAdjacencyMatrix(n);
+	printAdjacencyMatrix(n);
+	while(1)
+	{
+		printf("\n1.BFS   2.DFS\n");
+		scanf("%d",&ch);
+		printf("Enter starting vertex:  ");
+		scanf("%d",&v);
+		switch (ch)
+		{
+		case 1:
+			f = 0, r = -1, count = 0;
+			clearVisited();
+			BFS(--v, n);
+			printf("\n\n");
+			break;
+		case 2:	DFS(--v,n);
+			clearVisited();
+			printf("\n\n");
+			break;
+		}
+	}
+	
+	return 0;
 }
